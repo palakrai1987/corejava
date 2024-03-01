dd153f8ac11f670eb9522b22ca172b90cc929d36
palakrai1987 <its.palak11@gmail.com>
2024-03-01 19:31:19 +0530

    "2nd day"

---
+++ src/firstprogram.java
@@ -0,0 +1,18 @@
+
+
+public class firstprogram {
+
+	 public static void main(String[] args) {
+		
+		 int age = 10;
+		 String name = "palak";
+		
+		System.out.println("hello world %d");
+		System.out.printf(" age is %d name is %15S",age,name);
+		
+		// TODO Auto-generated method stub
+
+	}
+
+}
+ 
